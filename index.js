1cb91da59c42280382ed832a28f4b5992e635322
MegaTKC <59368582+MegaTKC@users.noreply.github.com>
2023-11-15 19:16:35 -0800

    make corrosion work behind the scenes

--- index.js
+++ index.js
@@ -14,20 +14,19 @@ proxy.bundleScripts();
 server.on('request', (request, response) => {
     const url = request.url;
 
-    // Check if the request URL starts with the specified prefix
     if (url.startsWith(proxy.prefix)) {
         return proxy.request(request, response);
     }
 
-    // Check if the request is for style.css
     if (url === '/style.css') {
         const cssPath = path.join(__dirname, '/style.css');
         response.writeHead(200, { 'Content-Type': 'text/css' });
         return fs.createReadStream(cssPath).pipe(response);
     }
 
-    // For other requests, serve the index.html file
     const indexPath = path.join(__dirname, '/index.html');
-    response.writeHead(200, { 'Content-Type': 'text/html' }); // Set Content-Type to 'text/html'
-    response.end(fs.readFileSync(indexPath, 'utf-8'));
+    const indexContent = fs.readFileSync(indexPath, 'utf-8');
+    
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(indexContent);
 }).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(3000);
