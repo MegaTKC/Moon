df70b70566783546cde0ff55abc11b9f26db7fc8
MegaTKC <59368582+MegaTKC@users.noreply.github.com>
2023-11-16 22:10:17 -0800

    fix the code so it doesnt override corrosion

--- public/switcher.js
+++ public/switcher.js
@@ -2,14 +2,14 @@ function toggleProxy() {
     var form = document.getElementById('proxy-form');
     var proxyType = document.getElementById('proxy-type');
 
-    if (form.action.includes('/corr/')) {
-        // Switch to Node-Unblocker proxy
-        form.action = '/nub/gateway/';
-        proxyType.innerText = 'Node-Unblocker';
-    } else {
+    if (form.action.includes('/nub/')) {
         // Switch to Corrosion proxy
         form.action = '/corr/gateway/';
         proxyType.innerText = 'Corrosion';
+    } else {
+        // Switch to Node-Unblocker proxy
+        form.action = '/nub/';
+        proxyType.innerText = 'Node-Unblocker';
     }
 }
 
@@ -19,21 +19,19 @@ function submitForm() {
 
     if (form.action.includes('/nub/') && urlInput !== '') {
         // Check if the input is a valid URL
-        if (isValidURL(urlInput)) {
+        if (!isValidURL(urlInput)) {
+            // If it's not a URL and using Node-Unblocker, perform a Google search
+            form.action = '/nub/' + 'https://www.google.com/search?q=' + encodeURIComponent(urlInput);
+        } else {
             // If it's a valid URL, use it as is without encoding
             form.action = '/nub/' + urlInput;
-        } else {
-            // If it's not a URL, default to https:// and append it to the action
-            form.action = '/nub/' + 'https://' + encodeURIComponent(urlInput);
         }
-    } else {
-        // If using 'Unblocker' and the input is plain text, perform a Google search
-        form.action = 'https://www.google.com/search?q=' + encodeURIComponent(urlInput);
     }
 
     // Submit the form
     form.submit();
 }
+
 
 function isValidURL(url) {
     // Use a simple regex pattern to check if the input is a valid URL
