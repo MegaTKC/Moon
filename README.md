090fcaedba9e389844b835f4c98a294eef2728dc
MegaTKC <59368582+MegaTKC@users.noreply.github.com>
2023-11-18 13:04:09 -0800

    COMMIT ALL THE CODE

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Moon
-A simple frontend for evading web proxies/internet censorship.
+A frontend with multiple proxies (coming soon) and enhancements so you can browse the web freely, without censorship or web filters imposed by the School (District) or workplace. 
+
+## Proxies included
+Below are the proxies currently included in Moon.
+
+### Ultraviolet
+Ultraviolet is a highly advanced proxy using service workers and TompHTTP. Created by TitaniumNetwork and offers regular updates as well as support for CAPTCHA and many of the most popular websites. (Instagram, YouTube, Discord, Spotify, etc.) It could also get your system theme!
+
+### Corrosion
+Corrosion is a proxy designed by TitaniumNetwork which was succeeded by Ultraviolet and the successor to the Alloy proxy. Corrosion supports a large majority of websites, including YouTube (and YouTube Music), Soundcloud, and Coolmath Games!
+
+## Libaries & Credits
+Credits to `expressjs` for serving my static files. (HTML/CSS/JS)<br>
+- `TitaniumNetwork` for creating Corrosion and Ultraviolet. (I basically just modified Ultraviolet-App, added Corrosion, add some stuffs and remove stuff I didn't want.)
+- `@tomphttp/bare-server-node` Creating an interesting future for proxies because they have service workers.
---
+++ index.js
@@ -0,0 +1,83 @@
+import express from "express";
+import { createServer } from "node:http";
+import { publicPath } from "ultraviolet-static";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { join } from "node:path";
+import { hostname } from "node:os";
+import Corrosion from "corrosion";
+import { createBareServer } from "@tomphttp/bare-server-node";
+
+const bare = createBareServer("/bare/");
+const app = express();
+
+const corrosion = new Corrosion({
+  codec: 'xor',
+  prefix: '/co/',
+});
+
+// Load our publicPath first and prioritize it over UV.
+app.use(express.static(publicPath));
+// Load vendor files last.
+// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
+app.use("/uv/", express.static(uvPath));
+
+// Error for everything else
+app.use((req, res) => {
+  res.status(404);
+  res.sendFile(join(publicPath, "404.html"));
+});
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else if (req.url.startsWith(corrosion.prefix)) {
+    corrosion.request(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else if (req.url.startsWith(corrosion.prefix)) {
+    corrosion.upgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+server.on("listening", () => {
+  const address = server.address();
+
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
+});
+
+corrosion.bundleScripts();
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close();
+  bare.close();
+  process.exit(0);
+}
+
+server.listen({
+  port,
+});
